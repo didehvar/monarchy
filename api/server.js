c79867b4f0c1c9a060968ca4182320af72519a44
James Didehvar <james@didehvar.com>
2014-09-12 04:22:38 +0100

    was sleepy, forgot to make commits :(
    saving incase i mess something up while i get more sleepy ;)

--- api/server.js
+++ api/server.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var app = express();
+var i18n = require('i18n');
+var validator = require('./utility/vldr');
 
-app.get('/api/users/:username', function(req, res, next) {
-  res.json({
-    "user": {
-      "id": "0",
-      "username": "elf",
-      "email": "elf@elf.com",
-      "password": "secret"
-    }
-  });
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://test:e5n$BoWO@ds033380.mongolab.com:33380/monarchy');
+
+i18n.configure({
+  locales: ['en'],
+  directory: __dirname + '/locales',
+});
+
+app.use(require('body-parser').json());
+app.use(require('cookie-parser')());
+app.use(i18n.init);
+app.use(validator.init);
+
+// controllers
+[
+  'auth/login',
+  'user'
+].forEach(function(path) {
+  require('./controllers/' + path)(app);
 });
 
 var server = app.listen(3000, function() {
