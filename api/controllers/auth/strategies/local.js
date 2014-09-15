cd5a7c545408a7fff72db792623e39863e13ddeb
James Didehvar <james@didehvar.com>
2014-09-15 21:11:49 +0100

    use restify instead of express

--- api/controllers/auth/strategies/local.js
+++
@@ -1,8 +0,0 @@
-var passport = require('passport'),
-    local = require('passport-local').Strategy;
-
-// passport.use(new local(
-//   function(username, password, done) {
-//     User.findOne({})
-//   }
-// ));
---
+++ api/restify.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var bunyan = require('bunyan');
+
+var log = bunyan.createLogger({ name: 'monarchy' });
+
+var server = restify.createServer({
+  version: '0.0.1',
+  log: log
+});
+
+var count = 0;
+server.use(function foo(req, res, next) {
+  count++;
+  next();
+});
+
+function respond(req, res, next) {
+  req.log.info('test');
+  res.send({ name: req.params.name, count: count });
+  next();
+}
+
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
