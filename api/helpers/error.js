d1deb1c6e92ea7225349042f5368a22bdcb003f0
James Didehvar <james@didehvar.com>
2014-09-16 05:38:50 +0100

    Revert "mongo error helper now returns"
    This reverts commit a0213468282cd01b82e2e1d2d648110abb74ad8c.

--- api/helpers/error.js
+++ api/helpers/error.js
@@ -6,11 +6,7 @@ exports.appendMongo = function helperAppendMongo(error, errorGroup) {
     return;
   }
 
-  var errorGroup = (typeof errorGroup === 'undefined') ? {} : errorGroup;
-
   _.each(error.errors, function(e) {
     return errorGroup[e.path] = e.message;
   });
-
-  return errorGroup;
 }
