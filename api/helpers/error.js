a0213468282cd01b82e2e1d2d648110abb74ad8c
James Didehvar <james@didehvar.com>
2014-09-16 05:38:24 +0100

    mongo error helper now returns
    useful for just working with an isolated set of errors

--- api/helpers/error.js
+++ api/helpers/error.js
@@ -6,7 +6,11 @@ exports.appendMongo = function helperAppendMongo(error, errorGroup) {
     return;
   }
 
+  var errorGroup = (typeof errorGroup === 'undefined') ? {} : errorGroup;
+
   _.each(error.errors, function(e) {
     return errorGroup[e.path] = e.message;
   });
+
+  return errorGroup;
 }
